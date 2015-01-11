a714c3823a6842ab502fc05da6c1b3c43a720390
Luca Subiaco <subluca@gmail.com>
2015-01-11 21:42:50 +0100

    verion 0.2

--- script/forms.py
+++
@@ -1,7 +0,0 @@
-from wtforms import *
-import wtforms
-from wtforms.validators import DataRequired
-print dir(wtforms)
- 
-class ContactForm(Form):
-    name = StringField('name', validators=[DataRequired()])
--- script/setup_area.py
+++
@@ -1,8 +0,0 @@
-from flask import *
-import sys
-
-class Main:
-
-    
-
-Main()
