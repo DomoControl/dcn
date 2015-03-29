e1e2935d6265d025a928d4e8c27d1898175f4330
lucasub <subluca@gmail.com>
2015-03-29 10:35:56 +0000

    update

--- date.py
+++ date.py
@@ -1,7 +1,6 @@
 import datetime
 
-legal_time = 2
-
+legal_time = 2 #Legal time difference
 
 def now():
     return datetime.datetime.now() + datetime.timedelta(hours = legal_time)
--- web.py
+++ web.py
@@ -570,6 +570,6 @@ if __name__ == '__main__':
         app.run(host="0.0.0.0", port=5000, debug=False)
     except:
     #~ except InvalidCommand as err:
-        print("*** Error: {}".format(err))
+        #~ print("*** Error: {}".format(err))
         pass
 
